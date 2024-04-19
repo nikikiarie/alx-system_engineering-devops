b88b9006be17a510af5e051b76c3933c9c19a125
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-19 13:40:21 -0700

    ident

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Creates file
 
 file { '/tmp/school':
-	ensure => file,
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
-
+  ensure  =>  file,
+  mode  =>  '0744',
+  owner  =>  'www-data',
+  group  =>  'www-data',
+  content  =>  'I love Puppet',
 }
