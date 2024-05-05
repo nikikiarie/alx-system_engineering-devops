abb86c4cb518fb83b86ab36323c2f8408a42dbce
nikikiarie <nix.kiariebiz@gmail.com>
2024-05-05 11:41:30 -0700

    puppet manifest

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+# Add a custom HTTP header with Puppet
+
+exec { 'update:
+  command => '/usr/bin/apt-get update',
+}
+
+->package { 'nginx':
+  ensure => 'present',
+}
+
+->file_line { 'header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => 'http {\n\tadd_header X-Served-By "${hostname}"}',
+}
+
+->exec { 'restart':
+  command => '/usr/sbin/service nginx restart',
+}
+
